da5818b64544d72634de4ea653beef0cc27e8516
Siva Palakurthi <sivapalakurthi@seers.in>
2014-03-07 00:51:18 +0530

    some more pre-processing

--- execute.m
+++ execute.m
@@ -1,12 +1,16 @@
 % load data using import Data..... in variables urls, datesUnix, ids 
 % in datafile browser.csv.... replaced ',' in urls with (comma)
 
-for i = 1 : length(datesUnix)
-    dates(i,:) = loadData(datesUnix(i));
-end
+%dates=cell(length(datesUnix),1);
+%size(dates)
 
 pattern = '[//]*//[^/]*';
-for i = 1 : length(urls)
+for i = 1 : length(datesUnix)
+    %dates{i} = num2cell(loadData(datesUnix(i)));
+    dates{i,:} = loadData(datesUnix(i));
+ 
     temp = regexp(urls(i), pattern, 'match');
-   urlsTrunc(i,:) = temp{1}(1);
+    urlsTrunc(i,:) = temp{1}(1);
 end
+ides = num2cell(1 : length(datesUnix))';
+data = [ides urlsTrunc dates];
--- loadData.m
+++ loadData.m
@@ -5,4 +5,5 @@ function tm = loadData(tu)
 time_unix =  tu;
 time_reference = datenum('1970', 'yyyy');
 time_matlab = time_reference + time_unix / 8.64e7;
-tm = datestr(time_matlab, 'yyyymmdd HH:MM:SS.FFF');
+time_str = datestr(time_matlab, 'yyyymmdd HH:MM:SS.FFF');
+tm = datevec(time_str);
